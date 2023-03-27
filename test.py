fce71f5b1a776c8a487fb649cdd7f1621deb53c4
yonas <115009469+yonass08@users.noreply.github.com>
2023-03-27 21:58:05 +0300

    implement testing

--- test.py
+++ test.py
@@ -1,17 +1,63 @@
 import unittest
 from string_functions import *
 
+
 class TestToUpper(unittest.TestCase):
     # write your code here
-    pass
+    def test_to_upper_type(self):
+        # check that to_upper fails when the input is not a string
+        string = 2.0
+        with self.assertRaises(TypeError):
+            to_upper(string)
+
+    def test_to_upper_functionality(self):
+        # Arrange
+        string = "Hi, Abebe"
+
+        # Act
+        res = to_upper(string)
+
+        # Assert
+        self.assertEquals(res, string.upper())
+
 
 class TestToLower(unittest.TestCase):
     # Write you code here
-    pass
+    def test_to_lower_type(self):
+        # check that to_lower fails when the input is not a string
+        string = 2.0
+        with self.assertRaises(TypeError):
+            to_lower(string)
+
+    def test_to_upper(self):
+        # Arrange
+        string = "HiAbebe"
+
+        # Act
+        res = to_lower(string)
+
+        # Assert
+        self.assertEquals(res, string.lower())
+
 
 class TestCapitalize(unittest.TestCase):
     # Write your code here
-    pass
+    def test_capitalize_type(self):
+        # check that capitalize fails when the input is not a string
+        string = 2.0
+        with self.assertRaises(TypeError):
+            to_upper(string)
+
+    def test_capitalize(self):
+        # Arrange
+        string = "hi, Abebe"
+
+        # Act
+        res = capitalize(string)
+
+        # Assert
+        self.assertEquals(res, string.capitalize())
+
 
 if __name__ == '__main__':
     unittest.main()
